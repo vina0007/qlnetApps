c8e48e560a39ac6a018e464ac14475259a8eab79
vina0007 <jason.vinar@gmail.com>
2016-04-21 21:00:29 -0700

    first commit

---
+++ qlnetMBS/qlnetMBS/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace qlnetMBS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double wac;
+            int wala;
+            int wam;
+            DateTime factorDate;
+            double factor;
+            double originalFace;
+            double currentFace;
+            int statedDelay;
+            double netCoupon;
+            string secType;
+            DateTime settleDate;
+
+            double yield_be;
+            double price;
+
+            string prepaymodel;
+
+
+
+        }
+    }
+}
