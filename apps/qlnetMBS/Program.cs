19d8f23b5d28dbbb58316efdf0fb30e36b5f7a35
vina0007 <jason.vinar@gmail.com>
2016-05-02 23:11:38 -0700

    general setup complete need to debug discounting... slightly different price wac and pass thru rate in class switched need to add payment delay nice to add sec type (PT, PO, IO)

--- apps/qlnetMBS/Program.cs
+++ apps/qlnetMBS/Program.cs
@@ -11,34 +11,60 @@ namespace qlnetMBS
     {
         static void Main(string[] args)
         {
-            Date referenceDate = new Date(29, 4, 2016);
-            int settlementDays = 2;
+            /*
+             * TODO:
+             *  1. Add delay
+             *  2. Add SecType enum PT, PO, IO
+             *  3. WAC vs Net Coupon (meanings are reversed)
+             * 
+             */
+
+            Date referenceDate = new Date(12, 11, 2015);
+            Settings.setEvaluationDate(referenceDate);
+            int settlementDays = 0;
             Calendar calendar = new TARGET();
             int origTerm = 360;
             Frequency sinkingFrequency = Frequency.Monthly;
             DayCounter accrualDayCounter = new Thirty360();
+            BusinessDayConvention paymentConvention = BusinessDayConvention.Unadjusted;
 
-            double wac = 0.04;
-            int wala;
-            int wam = 324;
-            Date factorDate = new Date(1, 5, 2016);
-            double factor;
-            //double originalFace;
+
+            double wac = 0.035;
+            int wam = 354;
+            int wala = origTerm - wam;
+            Date factorDate = new Date(1, 11, 2015);
+            Date issueDate = calendar.advance(factorDate, -wala, TimeUnit.Months, BusinessDayConvention.Unadjusted);
+            double factor = 1.0;
             double currentFace = 1000000;
-            int statedDelay;
-            double netCoupon = 0.035;
+            double originalFace = currentFace / factor;
+            int statedDelay = 0; //54;
+            double netCoupon = 0.04;
             string secType;
             Date settleDate;
 
-            double yield_be = 0.027;
+            double yield_be = 0.035;
             //double price;
             
-            double cpr = 0.08;
+            double cpr = 0.07;
+            double psa = 1;
 
-            IPrepayModel iPrepayModel = new ConstantCPR(cpr);
+            IPrepayModel constantcpr = new ConstantCPR(cpr);
+            IPrepayModel psacurve = new PSACurve(factorDate, psa);
 
-            MBSFixedRateBond mbs = new MBSFixedRateBond(settlementDays, calendar, currentFace, factorDate,
-                new Period(wam, TimeUnit.Months), new Period(origTerm, TimeUnit.Months), sinkingFrequency, wac, netCoupon, accrualDayCounter, iPrepayModel);
+            MBSFixedRateBond mbs = new MBSFixedRateBond(
+                settlementDays, 
+                calendar, 
+                currentFace, 
+                factorDate,
+                new Period(wam, TimeUnit.Months), 
+                new Period(origTerm, TimeUnit.Months), 
+                sinkingFrequency,
+                wac,
+                netCoupon,
+                accrualDayCounter,
+                psacurve,
+                paymentConvention,
+                issueDate);
 
             YieldTermStructure discountCurve = new FlatForward(referenceDate, yield_be, new Thirty360(), Compounding.Compounded, Frequency.Semiannual);
 
@@ -46,7 +72,24 @@ namespace qlnetMBS
 
             mbs.setPricingEngine(discountingBondEngine);
 
-            Console.WriteLine(mbs.cleanPrice());
+            Console.WriteLine("clean price: {0:F6}", mbs.cleanPrice());
+            Console.WriteLine("dirty price: {0:F6}", mbs.dirtyPrice());
+            Console.WriteLine("accrued int: {0:F6}", mbs.accruedAmount());
+
+            Console.WriteLine(discountCurve.discount(new Date(1, 12, 2015)));
+            Console.WriteLine(discountCurve.discount(new Date(1, 1, 2016)));
+            Console.WriteLine(discountCurve.discount(new Date(1, 2, 2016)));
+            Console.WriteLine(discountCurve.discount(new Date(1, 3, 2016)));
+
+            foreach (CashFlow cf in mbs.expectedCashflows())
+            {
+                if (cf.GetType() == typeof(VoluntaryPrepay))
+                    Console.WriteLine("upmt: {0:F2} --> {1:mm/dd/yyy}", cf.amount(), cf.date());
+                if (cf.GetType() == typeof(AmortizingPayment))
+                    Console.WriteLine("ppmt: {0:F2} --> {1:mm/dd/yyy}", cf.amount(), cf.date());
+                if (cf.GetType() == typeof(FixedRateCoupon))
+                    Console.WriteLine("ipmt: {0:F2} --> {1:mm/dd/yyy}", cf.amount(), cf.date());
+            }
 
         }
     }
