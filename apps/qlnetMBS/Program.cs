1f346c934da7d5f150f24792bce704a1cc6e43a6
vina0007 <jason.vinar@gmail.com>
2016-05-03 21:54:08 -0700

    created cash flow output and validated against known output methods to calculate price depend on cash flow instead of expected cash flow and therefore gives the wrong price

--- apps/qlnetMBS/Program.cs
+++ apps/qlnetMBS/Program.cs
@@ -16,7 +16,7 @@ namespace qlnetMBS
              *  1. Add delay
              *  2. Add SecType enum PT, PO, IO
              *  3. WAC vs Net Coupon (meanings are reversed)
-             * 
+             *  4. replace cash flows with expected cash flows to get the correct price 
              */
 
             Date referenceDate = new Date(12, 11, 2015);
@@ -38,7 +38,7 @@ namespace qlnetMBS
             double currentFace = 1000000;
             double originalFace = currentFace / factor;
             int statedDelay = 0; //54;
-            double netCoupon = 0.04;
+            double netCoupon = 0.035;
             string secType;
             Date settleDate;
 
@@ -72,25 +72,73 @@ namespace qlnetMBS
 
             mbs.setPricingEngine(discountingBondEngine);
 
+            double cp = mbs.cleanPrice();
+
             Console.WriteLine("clean price: {0:F6}", mbs.cleanPrice());
             Console.WriteLine("dirty price: {0:F6}", mbs.dirtyPrice());
             Console.WriteLine("accrued int: {0:F6}", mbs.accruedAmount());
 
-            Console.WriteLine(discountCurve.discount(new Date(1, 12, 2015)));
-            Console.WriteLine(discountCurve.discount(new Date(1, 1, 2016)));
-            Console.WriteLine(discountCurve.discount(new Date(1, 2, 2016)));
-            Console.WriteLine(discountCurve.discount(new Date(1, 3, 2016)));
+            // month, factor, pay date, ending prin, interest, reg principal, prepaid principal, total principal, net flow, cpr, smm, wala, wam, p&i payment, i payment, beg balance, days, discount, pv
+            double ebal = currentFace;
 
-            foreach (CashFlow cf in mbs.expectedCashflows())
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("output.csv"))
             {
-                if (cf.GetType() == typeof(VoluntaryPrepay))
-                    Console.WriteLine("upmt: {0:F2} --> {1:mm/dd/yyy}", cf.amount(), cf.date());
-                if (cf.GetType() == typeof(AmortizingPayment))
-                    Console.WriteLine("ppmt: {0:F2} --> {1:mm/dd/yyy}", cf.amount(), cf.date());
-                if (cf.GetType() == typeof(FixedRateCoupon))
-                    Console.WriteLine("ipmt: {0:F2} --> {1:mm/dd/yyy}", cf.amount(), cf.date());
-            }
+                DayCounter dc = discountCurve.dayCounter();
+                Date refdate = discountCurve.referenceDate();
 
+                 
+
+                sw.WriteLine("month,factor date,factor,pay date,ending principal,interest,regular principal,prepaid principal,total principal,net flow,cpr,smm,wala,wam,p&i payment,interest payment,beginning balance,days,discount,pv");
+                for (int i = 0; i < wam; i++)
+                {
+                    double upmt = 0;
+                    double ppmt = 0;
+                    double ipmt = 0;
+                    double bbal = ebal;
+
+                    Date paydate = null;
+
+                    for (int j = 0; j <= 2; j++)
+                    {
+                        int k = i * 3 + j;
+                        CashFlow cf = mbs.expectedCashflows()[k];
+                        if (cf.GetType() == typeof(VoluntaryPrepay))
+                        {
+                            upmt = cf.amount();
+                            paydate = cf.date();
+                        }
+                        if (cf.GetType() == typeof(AmortizingPayment))
+                            ppmt = cf.amount();
+                        if (cf.GetType() == typeof(FixedRateCoupon))
+                            ipmt = cf.amount();
+                    }
+                    int days = dc.dayCount(refdate, paydate);
+                    double df = discountCurve.discount(paydate);
+                    ebal = bbal - upmt - ppmt;
+                    double smm = upmt / (bbal - ppmt);
+
+                    sw.Write("{0},", i + 1); //month
+                    sw.Write("{0},", calendar.advance(factorDate, i,TimeUnit.Months,BusinessDayConvention.Unadjusted)); //factor date
+                    sw.Write("{0:F10},", factor * bbal / currentFace); //factor
+                    sw.Write("{0},", paydate.ToShortDateString()); //pay date
+                    sw.Write("{0:F2},", ebal); //ending principal
+                    sw.Write("{0:F2},", ipmt); //interest
+                    sw.Write("{0:F2},", ppmt); //regular principal
+                    sw.Write("{0:F2},", upmt); //prepaid principal
+                    sw.Write("{0:F2},", ppmt + upmt); //total principal
+                    sw.Write("{0:F2},", ipmt + ppmt + upmt); //net flow
+                    sw.Write("{0:F4},", 1 - Math.Pow(1 - smm, 12)); //cpr
+                    sw.Write("{0:F6},", smm); //smm
+                    sw.Write("{0},", wala + i); //wala
+                    sw.Write("{0},", wam - i); //wam
+                    sw.Write("{0},", i); //p&i payment
+                    sw.Write("{0},", i); //interest payment
+                    sw.Write("{0:F2},", bbal); //beginning balance
+                    sw.Write("{0},", days); //days
+                    sw.Write("{0:F8},", df); //discount
+                    sw.WriteLine("{0}", df * (ipmt + ppmt + upmt)); //pv
+                }
+            } 
         }
     }
 }
